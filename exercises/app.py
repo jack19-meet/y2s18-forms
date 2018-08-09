208bddf69e32d26022033c755f593cd17b853bdb
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 12:13:15 +0300

    part 3 done

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,9 +10,24 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
-@app.route('/add')
+@app.route('/add', methods = ["GET", "POST"])
 def add_student_route():
-    return render_template("add.html")
+    if request.method == "GET":
+        return render_template("add.html")
+   
+    else:
+        name = request.form["student_name"]
+        year = request.form["student_year"]
+        lab_t = request.form["lab"]
+        if lab_t = "True":
+            add_student(name, year, True)
+        else:
+            add_student(name, year, False)
 
 
+        add_student = [name, year]
+
+        return redirect('/')
+        session.add()
+        session.commit()
 app.run(debug=True)
